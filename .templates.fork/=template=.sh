2c83851cc58a7876eae3c1fc97fff610570a8dd9
qingzew <qingzew@gmail.com>
2017-03-25 14:59:38 +0800

    Create =template=.sh

---
+++ .templates.fork/=template=.sh
@@ -0,0 +1,19 @@
+/*************************************************************
+ * Copyright (C) %YEAR% %COMPANY% All Rights Reserved
+ *
+ * Distributed under terms of the %LICENSE% license.
+ *************************************************************/
+
+/**
+ * @file %FFILE%
+ * @author %USER%
+ * @date %FDATE%
+ * @brief
+ *
+ **/
+
+
+%HERE%
+
+
+/*set sw=4 ts=4 sts=4 et tw=78 */
