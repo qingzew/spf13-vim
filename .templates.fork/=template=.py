61393c87ee3c1c533c5dd74f4de3af677046f40f
qingzew <qingzew@gmail.com>
2017-03-30 11:41:20 +0800

    Update =template=.py

--- .templates.fork/=template=.py
+++ .templates.fork/=template=.py
@@ -2,22 +2,17 @@
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
 
-/*************************************************************
- * Copyright (C) %YEAR% %COMPANY% All Rights Reserved
- *
- * Distributed under terms of the %LICENSE% license.
- *************************************************************/
+###############################################################
+# Copyright (C) %YEAR% %COMPANY% All Rights Reserved
+#
+# Distributed under terms of the %LICENSE% license.
+###############################################################
 
-/**
- * @file %FFILE%
- * @author %USER%
- * @date %FDATE%
- * @brief 
- *  
- **/
-
+# @file %FFILE%
+# @author %USER%
+# @date %FDATE%
+# @brief
 
 %HERE%
 
-
-/*set sw=4 ts=4 sts=4 et tw=78 */
+# set sw=4 ts=4 sts=4 et tw=78
