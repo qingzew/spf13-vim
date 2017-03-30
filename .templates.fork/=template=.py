a68d33d5cd703ac835c2fa5fd9c2686955e7ab64
qingzew <qingzew@gmail.com>
2017-03-30 11:32:34 +0800

    Update =template=.py

--- .templates.fork/=template=.py
+++ .templates.fork/=template=.py
@@ -1,3 +1,7 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
 /*************************************************************
  * Copyright (C) %YEAR% %COMPANY% All Rights Reserved
  *
